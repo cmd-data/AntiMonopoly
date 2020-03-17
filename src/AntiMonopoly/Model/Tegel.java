39ee712b51f89211adfa03dd682d938065413516
cmd-data <tchartouh@gmail.com>
2020-03-17 19:51:25 +0100

    Template nieuwe tegelMethode() om cases op te splitsen

--- src/AntiMonopoly/Model/Tegel.java
+++ src/AntiMonopoly/Model/Tegel.java
@@ -1,5 +1,18 @@
 package AntiMonopoly.Model;
 
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Tegel {
@@ -8,12 +21,15 @@ public class Tegel {
 	private String naam;
 	private List<Speler> spelers;
 
+	Rectangle pion = null;
+	Speler aanZet = null;
+
 	public Tegel(String naam, int positie) {
 		this.naam = naam;
 		this.positie = positie;
 	}
 
-	public void tegelMethode(Tegel tegel, Speler speler) {
+	/*public void tegelMethode(Tegel tegel, Speler speler) {
 		int soortVak = tegel.positie;
 
 		if (kopen(tegel)) {
@@ -56,7 +72,7 @@ public class Tegel {
 						Eigendomsbelasting.betaalBelasting(speler);
 				}
 			}
-	}
+	}*/
 
 	public void kopen(){}
 
@@ -134,6 +150,62 @@ public class Tegel {
 		Spel.updateGeld(speler);
 	}
 
+	public void tegelMethode(Rectangle pion, Speler aanZet) {
+
+		List<Rectangle> pionnen = new ArrayList<>();
+
+		for (int i = 0; i < Spel.getSpelers().size(); i++) {
+			pionnen.add(Spel.getSpelers().get(i).getRectangle());
+		}
+
+		switch (Dice.getCount()) {
+			case 1:
+				pion = pionnen.get(0);
+				aanZet = Spel.getSpelers().get(0);
+				break;
+			case 2:
+				pion = pionnen.get(1);
+				aanZet = Spel.getSpelers().get(1);
+				break;
+			case 3:
+				pion = pionnen.get(2);
+				aanZet = Spel.getSpelers().get(2);
+				break;
+			case 4:
+				pion = pionnen.get(3);
+				aanZet = Spel.getSpelers().get(3);
+		}
+
+		int locatie = Spel.move(aanZet, Arrays.stream(Dice.getWorp2()).sum()).getPositie();
+
+		switch (locatie) {
+
+			case 0: tegel0(pion,aanZet);
+		}
+	}
+
+	public void tegel0(Rectangle pion, Speler aanZet) {
+		TranslateTransition transition0 = new TranslateTransition();
+		transition0.setNode(pion);
+		transition0.setDuration(Duration.seconds(1));
+		transition0.setToX(0);
+		transition0.setToY(0);
+		//transition0.setCycleCount(Timeline.INDEFINITE);
+		transition0.setInterpolator(Interpolator.EASE_BOTH);
+		transition0.play();
+
+		final Stage dialog0 = new Stage();
+		dialog0.initModality(Modality.APPLICATION_MODAL);
+		VBox dialogVBox0 = new VBox();
+		dialog0.setTitle("Start");
+		dialogVBox0.getChildren().addAll(new Text("U staat op start."));
+		Scene dialogScene0 = new Scene(dialogVBox0, 300, 250);
+		dialogVBox0.setAlignment(Pos.CENTER);
+		dialogVBox0.setSpacing(10);
+		dialogVBox0.setStyle("-fx-font: 20px Tahoma");
+		dialog0.setScene(dialogScene0);
+		dialog0.show();
+	}
 
 	public int getPositie() {
 		return positie;
